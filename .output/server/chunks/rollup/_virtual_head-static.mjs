993404c8928e9aeba3a9f5818cc0b5c4a7f4ae24
Goofus <jbellido158@gmail.com>
2023-12-25 01:35:12 -0800

    Removed .nuxt

--- .output/server/chunks/rollup/_virtual_head-static.mjs
+++
@@ -1,4 +0,0 @@
-const _virtual__headStatic = {"headTags":"<meta charset=\"utf-8\">\n<title>The Murder Scene</title>\n<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n<meta name=\"description\" content=\"Garry's Mod community home to unique gmod servers and the number one Murder server.\">\n<meta property=\"og:image\" content=\"http://murderscene.net/images/logo.png\">\n<meta property=\"og:image:secure_url\" content=\"https://murderscene.net/images/logo.png\">\n<link rel=\"icon\" type=\"image/x-icon\" href=\"/images/favicon.png\">\n<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700\" crossorigin=\"\">","bodyTags":"","bodyTagsOpen":"","htmlAttrs":"","bodyAttrs":""};
-
-export { _virtual__headStatic as default };
-//# sourceMappingURL=_virtual_head-static.mjs.map
